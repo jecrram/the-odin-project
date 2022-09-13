af485619c9902b9db788a160702a2da3d68e5f16
Jericho <jericho.ramos108@gmail.com>
2022-09-13 19:43:58 +0800

    Edited css

--- Library Project/main.js
+++ Library Project/main.js
@@ -1,44 +1,51 @@
 const bookCardContainer = document.querySelector(".book-card-wrapper");
 
+// console.log(bookCardContainer)
 
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+//   // this.info = function () {
+//   //   return `${title} by ${author}. ${pages} pages, ${read}`
+//   // }
+// }
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
+// let myLibrary = [theHobbit, 'Michael\'s book', 'Bible']
 
-  this.info = function () {
-    return `${title} by ${author}. ${pages} pages, ${read}`
-  }
-}
+// function displayBook() {
+//   myLibrary.forEach((book) => {
+//     bookCardContainer.innerHTML += `
+//     <div class="book-card">
+//     <h3 class="book-card__title">${book.title}</h3>
+//     <p class="book-card__author">${book.author}</p>
+//     <p class="book-card__pages">${book.pages}</p>
+//     <p class="book-card__read"${book.read}></p>
+//     <button class="book-card__remove-btn">Remove</button>
+//   </div>
+//     `
+//   })
+// }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
-let myLibrary = [theHobbit, 'Michael\'s book', 'Bible']
+// function addBook() {
+//   console.log('hello')
+// }
 
+// function removeBook() {
 
+// }
 
-function displayBook() {
-  myLibrary.forEach((book) => {
-    bookCardContainer.innerHTML += `
-    <div class="book-card">
-    <h3 class="book-card__title">${book.title}</h3>
-    <p class="book-card__author">${book.author}</p>
-    <p class="book-card__pages">${book.pages}</p>
-    <p class="book-card__read"${book.read}></p>
-  </div>
-    `
-  })
-}
+// displayBook()
 
-displayBook()
+// const showAddBookForm = document.querySelector(".btn--add-book");
+// const bookFormModal = document.querySelector(".book-form-modal");
+// console.log(showAddBookForm)
 
-const showAddBookForm = document.querySelector(".btn--add-book");
-const bookFormModal = document.querySelector(".book-form-modal");
-console.log(showAddBookForm)
+// showAddBookForm.addEventListener('click', (e) => {
+//   console.log(e)
+//   bookFormModal.classList.toggle('active')
+// })
+// // console.log(myLibrary)
 
-showAddBookForm.addEventListener('click', (e) => {
-  console.log(e)
-  bookFormModal.classList.toggle('active')
-})
-// console.log(myLibrary)
