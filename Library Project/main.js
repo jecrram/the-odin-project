152eaf5a85dd5a7e8620b5011c6c6797c8f37938
Jericho <jericho.ramos108@gmail.com>
2022-09-13 12:31:07 +0800

    Added library project files

---
+++ Library Project/main.js
@@ -0,0 +1,44 @@
+const bookCardContainer = document.querySelector(".book-card-wrapper");
+
+
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    return `${title} by ${author}. ${pages} pages, ${read}`
+  }
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
+let myLibrary = [theHobbit, 'Michael\'s book', 'Bible']
+
+
+
+function displayBook() {
+  myLibrary.forEach((book) => {
+    bookCardContainer.innerHTML += `
+    <div class="book-card">
+    <h3 class="book-card__title">${book.title}</h3>
+    <p class="book-card__author">${book.author}</p>
+    <p class="book-card__pages">${book.pages}</p>
+    <p class="book-card__read"${book.read}></p>
+  </div>
+    `
+  })
+}
+
+displayBook()
+
+const showAddBookForm = document.querySelector(".btn--add-book");
+const bookFormModal = document.querySelector(".book-form-modal");
+console.log(showAddBookForm)
+
+showAddBookForm.addEventListener('click', (e) => {
+  console.log(e)
+  bookFormModal.classList.toggle('active')
+})
+// console.log(myLibrary)
