37b5495886734ba84c8cef9e511ed5a7111d9569
Jericho <jericho.ramos108@gmail.com>
2022-09-14 14:16:04 +0800

    Adding remove functionality

--- Library Project/main.js
+++ Library Project/main.js
@@ -3,7 +3,8 @@ const bookTitle = document.querySelector("#book-name");
 const bookAuthor = document.querySelector("#book-author");
 const bookPages = document.querySelector("#book-pages");
 const readStatus = document.querySelector("#read-status");
-const bookSubmit = document.querySelector(".book-form__submit-btn")
+const bookSubmit = document.querySelector(".book-form__submit-btn");
+const bookRemoveBtns = document.querySelectorAll(".book-card__remove-btn");
 
 let myLibrary = []
 
@@ -24,8 +25,11 @@ const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 542, 'Done reading'
 const lordOfTheRings = new Book('LOTR', 'J. R. R. Tolkien', '941', 'Still reading')
 const harryPotter = new Book('Harry Potter', 'J. K. Rowling', '302', 'Not yet read')
 
-
 myLibrary = [theHobbit, lordOfTheRings, harryPotter]
+
+
+console.log(myLibrary)
+
 
 bookSubmit.addEventListener('click', addBookToLibrary)
 
@@ -34,26 +38,52 @@ function addBookToLibrary(e) {
   e.preventDefault()
   let obj = new Book(bookTitle.value, bookAuthor.value, bookPages.value, readStatus.value)
   myLibrary.push(obj);
+
+  //Create
+  const div = document.createElement('div');
+  div.classList.add('book-card');
+  div.innerHTML = `
+  <h3 class="book-card__title">${obj.title}</h3>
+  <p class="book-card__author">${obj.author}</p>
+  <p class="book-card__pages">${obj.pages}</p>
+  <p class="book-card__read">${obj.read}</p>
+  <button class="book-card__remove-btn">Delete</button>`;
+  bookCardContainer.appendChild(div)
+
+  //Clear
+  bookTitle.value = "", bookAuthor.value = "", bookPages.value = null, readStatus.value = "";
 }
 
 console.log(myLibrary)
 
-window.addEventListener('click', (e) => {
-  displayBooks()
-})
+// window.addEventListener('onload', () => {
+//   displayBooks();
+// })
+
 
 function displayBooks() {
   return myLibrary.forEach(book => {
     console.log(book)
     const div = document.createElement('div');
-    div.classList.add('.book-card');
+    div.classList.add('book-card');
     div.innerHTML = `
     <h3 class="book-card__title">${book.title}</h3>
     <p class="book-card__author">${book.author}</p>
     <p class="book-card__pages">${book.pages}</p>
-    <p class="book-card__read">${book.readStatus}</p>`;
+    <p class="book-card__read">${book.read}</p>
+    <button class="book-card__remove-btn">Delete</button>`;
     bookCardContainer.appendChild(div)
   })
 }
 
-displayBooks()
+
+
+bookRemoveBtns.forEach((btn) => {
+  console.log(btn);
+})
+
+function removeBook() {
+
+}
+
+displayBooks();
