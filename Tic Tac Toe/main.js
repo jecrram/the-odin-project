ad1061bc6d174139d855cfd874db2b24a51a84a1
Jericho <jericho.ramos108@gmail.com>
2022-10-04 20:58:07 +0800

    Working tic tac toe

--- Tic Tac Toe/main.js
+++ Tic Tac Toe/main.js
@@ -1,70 +1,105 @@
 const gameBtns = document.querySelectorAll(".gameboard__btn");
+const restartBtn = document.querySelector("#restart-btn");
+const statusText = document.querySelector(".section__status")
 
-const player = (name, marker, playerTurn) => {
+const player = (name, marker, turn) => {
   name = name;
   marker = marker;
-  playerTurn = playerTurn;
-  let score = [];
+  turn = turn;
 
-  return { name, marker, playerTurn, score };
+  return { name, marker, turn };
 };
 
 const playerOne = player("Jericho", "fa-x", true);
 const playerTwo = player("Eriz", "fa-o", false);
 
 const gameBoard = (() => {
-  board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  let winningPatterns = [
-    [`1`, "2", "3"],
-    ["4", "5", "6"],
-    ["7", "8", "9"],
-    ["1", "4", "7"],
-    ["2", "5", "8"],
-    ["3", "6", "9"],
-    ["1", "5", "9"],
-    ["3", "5", "7"],
-  ];
+  const board = ["", "", "", "", "", "", "", "", ""]
+  let gameRunning = false;
+  let currentPlayer = playerOne;
+  let currentPlayerMarker = playerOne.marker;
 
-  const checkWinner = () => {
-    let playerWin;
-    winningPatterns.forEach((pattern) => {
-      pattern.every((num) => {
-        console.log(pattern, num);
-        // console.log(pattern);
-        // console.log(playerWin, num, playerOne.score);
-        // playerWin = playerOne.score.includes(num);
-      });
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  const initializeGame = () => {
+    gameRunning = true;
+
+    gameBtns.forEach((gameBtn) => {
+      gameBtn.addEventListener("click", cellClicked);
     });
-    return playerWin;
-  };
 
-  return { checkWinner };
-})();
+    restartBtn.addEventListener('click', restartGame);
+  }
 
-gameBoard.checkWinner();
+  console.log(checkWinner())
 
-const displayController = (() => {
-  const markTheSpot = (e) => {
-    if (playerOne.playerTurn) {
-      e.target.classList.add(`${playerOne.marker}`, `fa-solid`);
-      playerOne.playerTurn = false;
-      playerTwo.playerTurn = true;
-      playerOne.score.push(e.target.attributes["data-spot"].value);
-      // console.log(playerOne.score);
-      // console.log(playerTwo.score);
-    } else {
-      e.target.classList.add(`${playerTwo.marker}`, `fa-solid`);
-      playerOne.playerTurn = true;
-      playerTwo.playerTurn = false;
-      playerTwo.score.push(e.target.attributes["data-spot"].value);
-      // console.log(playerOne.score);
-      // console.log(playerTwo.score);
+  initializeGame();
+
+  function cellClicked() {
+    const cellIndex = this.getAttribute("data-index");
+
+    if (board[cellIndex] != "" || !gameRunning) {
+      return;
     }
-  };
 
-  return { markTheSpot };
-})();
+    updateCell(this, cellIndex);
+  }
 
-gameBtns.forEach((gameBtn) => {
-  gameBtn.addEventListener("click", displayController.markTheSpot);
-});
+  function updateCell(el, ind) {
+    board[ind] = currentPlayerMarker;
+    el.classList.add(currentPlayerMarker);
+    changePlayer()
+    checkWinner()
+
+  }
+
+  function restartGame() {
+
+  }
+
+  function changePlayer() {
+    currentPlayerMarker = (currentPlayerMarker === 'fa-x') ? 'fa-o' : 'fa-x';
+    currentPlayer = (currentPlayerMarker === 'fa-x') ? playerOne : playerTwo;
+    statusText.textContent = `${currentPlayer.name}'s Turn`
+  }
+
+  function checkWinner() {
+    let roundWon = false;
+
+    for (let i = 0; i < winPatterns.length; i++) {
+      const winCondition = winPatterns[i]; //[2,4,6]
+      const cellA = board[winCondition[0]]; // will check board[2]
+      const cellB = board[winCondition[1]]; // will check board[4]
+      const cellC = board[winCondition[2]]; // will check board[6]
+      console.log(cellA, cellB, cellC)
+
+      if (cellA == "" || cellB == "" || cellC == "") {
+        continue; //skips the iteration
+      }
+
+      if (cellA == cellB && cellB == cellC) {
+        console.log(cellA == cellB && cellB == cellC);
+        roundWon = true;
+        break; //breaks the entire for loop
+      }
+    }
+
+    if (roundWon) {
+      statusText.textContent = `${currentPlayer.name} wins!`;
+      gameRunning = false;
+    } else if (!board.includes("")) {
+      statusText.textContent = `Draw!`;
+      gameRunning = false;
+    }
+  }
+
+})()
