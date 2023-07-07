0bc10d6f736d6cbc26e9a99c26993e1f9d81e05c
Jericho <jericho.ramos108@gmail.com>
2023-07-07 17:10:19 +0800

    Fix rock-paper-scissors function

--- Rock Papers Scissors/main.js
+++ Rock Papers Scissors/main.js
@@ -1,9 +1,18 @@
 /*
 PSEUDO CODE
-Game starts with computer selecting a choice from array of rock, scissors, papers
+Computer selects hand
+Player selects hand
+if playerhand === pchand, then tie
+if player selects rock and pc selects scissors, then win
+if player selects rock and pc selects paper, then lose
+if player selects paper and pc selects scissors, then lose
+if player selects paper and pc selects rock, then win
+if player selects scissors and pc selects paper, then win
+if player selects scissors and pc selects rock, then lose
 */
 
 let choices = ["rock", "paper", "scissors"];
+let gameCount = 0;
 
 function getComputerChoice() {
   let random = Math.random();
@@ -12,21 +21,41 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let computerHand = computerSelection();
-  let playerHand = playerSelection;
-
-  if (computerHand === "rock" && playerHand === "rock") {
-    console.log("Tie");
-  } else if (computerHand === "rock" && playerHand === "paper") {
-    console.log("Computer wins");
-  } else if (computerHand === "rock" && playerHand === "scissors") {
-    console.log("Player wins");
+  //make sure that hands can be strictly checked with array
+  let playerHand = playerSelection.toLowerCase();
+  let computerHand = computerSelection.toLowerCase();
+  //when both players have same hand
+  if (playerHand === computerHand) {
+    return "It's a tie!";
+  } else if (
+    //conditions for player to win
+    (playerHand === "rock" && computerHand === "scissors") ||
+    (playerHand === "paper" && computerHand === "rock") ||
+    (playerHand === "scissors" && computerHand === "paper")
+  ) {
+    return "You win!";
+  } else {
+    //if not a winner, then you're a loser
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function game() {
+  for (let gameNumber = 1; gameNumber <= 5; gameNumber++) {
+    gameCount++;
+    //sets player and computer hands
+    let playerSelection = prompt("What are you playing?");
 
-// function game() {}
+    let computerSelection = getComputerChoice();
 
-playRound(playerSelection, computerSelection);
+    console.log(playerSelection, computerSelection, gameNumber);
+
+    if (gameNumber <= 5) {
+      return playRound(playerSelection, computerSelection);
+    } else {
+      return "Stop playing! You exceeded 5 games";
+    }
+  }
+}
+
+game();
