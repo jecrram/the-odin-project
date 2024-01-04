a7700f029c5c6fcb2f5705477d50b94bd3e65a16
Jericho <jericho.ramos108@gmail.com>
2024-01-04 08:06:49 +0800

    Fix grid square hover effect
    Fixed the grid square so it properly implements the hover effect

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,13 +1,21 @@
 let grid = document.querySelector(".container");
 let gridBtn = document.querySelector(".grid-btn");
 
-for (divCount = 0; divCount < 16; divCount++) {
+for (let divCount = 0; divCount < 16; divCount++) {
   let el = document.createElement("div");
-  grid.appendChild(el).className = "grid-item";
+  grid.appendChild(el).classList.add("flex-item");
 }
 
-gridBtn.addEventListener("click", () => {
-  let number;
-  number = window.prompt("How many squares do you want to add?");
-  console.log(number);
+let gridSquares = document.querySelectorAll(".flex-item");
+
+// gridBtn.addEventListener("click", () => {
+//   let number;
+//   number = window.prompt("How many squares do you want to add?");
+//   console.log(number);
+// });
+
+gridSquares.forEach((gridSquare) => {
+  gridSquare.addEventListener("mouseover", (el) => {
+    el.target.classList.toggle("flex-item--hover");
+  });
 });
