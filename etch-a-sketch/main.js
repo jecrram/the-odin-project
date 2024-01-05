c4d88f635d7ee7c15eeecd5d08aaf72b2fdf4db4
Jericho <jericho.ramos108@gmail.com>
2024-01-05 20:03:02 +0800

    Add background color change functionality

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,21 +1,59 @@
-let grid = document.querySelector(".container");
-let gridBtn = document.querySelector(".grid-btn");
+let resizeBtn = document.querySelector(".btn--resize");
+let sketchGrid = document.querySelector(".container");
 
-for (let divCount = 0; divCount < 16; divCount++) {
-  let el = document.createElement("div");
-  grid.appendChild(el).classList.add("flex-item");
+function colorCell() {
+  let cells = document.querySelectorAll(".cell");
+
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", (e) => {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      e.target.style.backgroundColor = `#${randomColor}`;
+    });
+  });
 }
 
-let gridSquares = document.querySelectorAll(".flex-item");
+resizeBtn.addEventListener("click", () => {
+  let gridSize;
 
-// gridBtn.addEventListener("click", () => {
-//   let number;
-//   number = window.prompt("How many squares do you want to add?");
-//   console.log(number);
-// });
+  gridSize = window.prompt("Enter a number between 1 to 100");
 
-gridSquares.forEach((gridSquare) => {
-  gridSquare.addEventListener("mouseover", (el) => {
-    el.target.classList.toggle("flex-item--hover");
-  });
+  createGrid(gridSize);
 });
+
+function buildInitialGrid() {
+  let gridSize = 16;
+  let numberOfCells = gridSize * gridSize;
+
+  for (let i = 0; i < numberOfCells; i++) {
+    let newCell = document.createElement("div");
+    newCell.classList.add("cell");
+
+    newCell.style.width = `calc(100% / ${gridSize})`;
+    newCell.style.height = `calc(100% / ${gridSize})`;
+
+    sketchGrid.appendChild(newCell);
+  }
+
+  colorCell();
+}
+
+function createGrid(gridSize) {
+  let numberOfCells = gridSize * gridSize;
+  console.log(numberOfCells);
+
+  sketchGrid.innerHTML = "";
+
+  for (let i = 0; i < numberOfCells; i++) {
+    let newCell = document.createElement("div");
+    newCell.classList.add("cell");
+
+    newCell.style.width = `calc(100% / ${gridSize})`;
+    newCell.style.height = `calc(100% / ${gridSize})`;
+
+    sketchGrid.appendChild(newCell);
+  }
+
+  colorCell();
+}
+
+document.addEventListener("DOMContentLoaded", buildInitialGrid);
